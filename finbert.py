a928f90a4d4eb8b7f18661be761523e7474aa829
A-els <32335567+A-els@users.noreply.github.com>
2021-04-01 10:29:53 +0100

    Update finbert.py

--- finbert.py
+++ finbert.py
@@ -285,11 +285,11 @@ class FinBert(object):
                           lr=self.config.learning_rate,
                           correct_bias=True)
 
-        #self.scheduler = get_linear_schedule_with_warmup(self.optimizer,
-                                                    #num_warmup_steps=self.num_warmup_steps,
-                                                    #num_training_steps=self.num_train_optimization_steps)
+        self.scheduler = get_linear_schedule_with_warmup(self.optimizer,
+                                                    num_warmup_steps=self.num_warmup_steps,
+                                                    num_training_steps=self.num_train_optimization_steps)
                 
-        self.scheduler = get_constant_schedule(self.optimizer) #using a constant scheduler for testing
+        #self.scheduler = get_constant_schedule(self.optimizer) #using a constant scheduler for testing
         
         return model
 
